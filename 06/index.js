518a3be162cc7d9cd2ae3743b0dab3894032bc9c
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-04 17:11:47 +0100

    zapis progresu klas i interfejsow, duzo tekstu

---
+++ 06/index.js
@@ -0,0 +1,335 @@
+/* Klasy i interfejsy */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//Klasa
+var User = /** @class */ (function () {
+    function User() {
+    }
+    User.prototype.sayHello = function () {
+        console.log("My name is " + this.name);
+    };
+    return User;
+}());
+//Wywołanie
+var x = new User();
+//Deklaracja klasy jest równiez deklaracją nowego typu User
+var x2 = new User();
+//Konstruktor
+//Klasy najczęściej posiadają zdefiniowany konstruktor
+var User2 = /** @class */ (function () {
+    function User2(name) {
+        this.name = name;
+    }
+    return User2;
+}());
+//Typescript posiada lukier składniowy upraszczający powyszy kod, oba przyklady są równowane
+var User3 = /** @class */ (function () {
+    function User3(name) {
+        this.name = name;
+    }
+    return User3;
+}());
+//Dzięki dodaniu słowa public w konstruktorze przed argumentem name
+//parametr zostanie automatycznie przypisany do instancji klasy w momencje jej tworzenia
+var x3 = new User3("Michał"); // user.name => 'Michał"
+var User4 = /** @class */ (function () {
+    function User4(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return User4;
+}());
+var x4 = new User4("Adam", 21); // user.name => "Adam", user.age => 21;
+//Pola bez inicjacji
+//TS poinformuje nas o pomyłce, jeśli zapomnimy zainicjować jakiegoś pola
+var User5 = /** @class */ (function () {
+    function User5() {
+    }
+    return User5;
+}());
+//Aby uniknąć błedu, powinniśmy oznaczyć pole jako opcjonalne
+var User6 = /** @class */ (function () {
+    function User6() {
+    }
+    return User6;
+}());
+//Jeśli chcemy stworzyć pole które nie jest opcjonalne, ale tez nie
+//jest od razu inicjalizowane, stosujemy kwalifikator  "!"
+//Ale nie jest to rekomendowane, TS nie będzie w stanie wyłapać pomyłki za nas.
+var User7 = /** @class */ (function () {
+    function User7() {
+    }
+    return User7;
+}());
+//Dziedziczenie
+//Klasy dziedziczą dzięki słowu kluczowemu extends
+var Vehicle = /** @class */ (function () {
+    function Vehicle(numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Car.prototype.ride = function () {
+        console.log("I have " + this.numberOfWheels + " wheels!");
+    };
+    return Car;
+}(Vehicle));
+var c = new Vehicle(4);
+//c.ride() // => I have 4 wheels! 
+//Jeśli subklasa równiez definiuje konstruktor, niezbędne
+//jest uzycie slowa kluczowego 'super'
+var A = /** @class */ (function () {
+    function A() {
+        this.a = "test A";
+    }
+    return A;
+}());
+var B = /** @class */ (function (_super) {
+    __extends(B, _super);
+    function B() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return B;
+}(A));
+var C = /** @class */ (function (_super) {
+    __extends(C, _super);
+    function C() {
+        return _super.call(this) || this;
+    }
+    return C;
+}(A));
+//Modyfikatory public, private, protected
+//Są podobne jak w innych językach, jednak TS wprowadza drobne róznice.
+//warta uwaga jest kompatybilność typów przy uzywaniu pól prywatnych
+//public
+//Oznacza, ze ma się dowolny dostęp do danego pola.
+//Jeśli nie oznaczymy zadnego modyfikatora, TS przyjmie defaultowo 'public'
+var D = /** @class */ (function () {
+    function D() {
+    }
+    return D;
+}());
+//private
+//Brak dostępu do danego pola z zewnątrz
+//Klasy dziedziczące nie mają do niego dostępu
+var User8 = /** @class */ (function () {
+    function User8(name) {
+        this.name = name;
+    }
+    User8.prototype.sayHello = function () {
+        console.log("Jestem " + this.name);
+    };
+    return User8;
+}());
+var user8 = new User8("Michał");
+//user8.sayHello(); // => Jestem Michał
+//user8.name; // => error przy kompilacji
+//protected
+//To samo co private tylko klasy dziedziczące mają dostęp
+var User9 = /** @class */ (function () {
+    function User9(name) {
+        this.name = name;
+    }
+    return User9;
+}());
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Admin.prototype.sayHello = function () {
+        console.log(this.name);
+    };
+    return Admin;
+}(User9));
+//const user9 = new Admin("Pablo");
+//user9.sayHello(); // => "Pablo"
+//readonly
+//Pola i metody mogą być oznaczone dodatkowo jako readonly, tylko do odczytu
+//to znaczy, ze nie mozliwe jest przypisanie ich poza konstruktorem
+//Zły przykład, cannot assign to state because read-only property
+var Component = /** @class */ (function () {
+    function Component() {
+        this.state = { userId: 123 };
+    }
+    Component.prototype.updateState = function () {
+        this.state = {};
+    };
+    return Component;
+}());
+//Readonly moze występować obok modyfikatorow dostępu
+var Component2 = /** @class */ (function () {
+    function Component2() {
+        this.state = { userId: 123 };
+    }
+    return Component2;
+}());
+//własności w parametrach konstruktora
+var User10 = /** @class */ (function () {
+    function User10(name) {
+        this.name = name;
+    }
+    return User10;
+}());
+//mozemy zamienic na
+var User11 = /** @class */ (function () {
+    function User11(name) {
+        this.name = name;
+    }
+    return User11;
+}());
+//Analogicznie mozna postepić z innymi modyfikatorami
+var User12 = /** @class */ (function () {
+    function User12(name) {
+        this.name = name;
+    }
+    return User12;
+}());
+//Klasa Abstrakcyjna
+//Są to klasy których nie da sie instancjonować i słuza wyłącznie do dziedziczenia po nich.
+//Klasy te deklarujemy tylko podajac typ i oznacza sie je rowniez slowem "abstract"
+var Message = /** @class */ (function () {
+    function Message(text) {
+        this.text = text;
+    }
+    ;
+    Message.prototype.log = function () {
+        console.log(this.text);
+    };
+    return Message;
+}());
+var Phone = /** @class */ (function (_super) {
+    __extends(Phone, _super);
+    function Phone() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Phone.prototype.sendSms = function () {
+        console.log('xD');
+    };
+    return Phone;
+}(Message));
+;
+var tel = new Phone('Hello moto');
+//Teraz mozna uzywać tego typu
+var invoice = {
+    amount: 123,
+    submittedAt: new Date()
+};
+//Kompatybilność typów
+function getInvoiceSummary(invoice) {
+    return ("Twoja faktura na kwote " + invoice.amount +
+        ("zosta\u0142a wys\u0142ana " + invoice.submittedAt));
+}
+getInvoiceSummary(invoice); //all good
+getInvoiceSummary({
+    amount: 42,
+    submittedAt: new Date()
+}); // all good
+getInvoiceSummary({
+    amount: 42,
+    submittedAt: new Date(),
+    blabla: 123
+}); //err
+var moreThanInvoice = {
+    amount: 42,
+    submittedAt: new Date(),
+    blabla: 123
+};
+getInvoiceSummary(moreThanInvoice); // all good!
+var ex = {
+    cannotUpdateMe: "abc"
+};
+ex.optionalProperty = 123; // OK => 123
+ex.cannotUpdateMe = "a"; //error
+//Mozliwe jest stworzenie tablicy tylko do odczytu
+//Wszystkie metody, modifikowanie jest niedozwolone w tego typu tablicy
+var arr = [1, 2, 3];
+arr[0] = 42; //err
+arr.push(123); //err
+arr.unshift(); //err
+var arr2 = [1, 2, 3];
+arr2[0] = 42; //ok
+arr2.push(123); //ok
+arr2.unshift(); //ok
+var ArrayQueue = /** @class */ (function () {
+    function ArrayQueue() {
+        this.array = [];
+    }
+    ArrayQueue.prototype.push = function (val) {
+        this.array.push(val);
+    };
+    ArrayQueue.prototype.pop = function () {
+        return this.array.pop();
+    };
+    return ArrayQueue;
+}());
+var arrQue = new ArrayQueue();
+arrQue.push('Apple');
+arrQue.push('Pineapple');
+arrQue.pop();
+arrQue; // ({array}) => "Apple" 
+//Klasy interfejsów mogą implementować wiele interfejsów:
+var MyClass = /** @class */ (function () {
+    function MyClass() {
+    }
+    return MyClass;
+}());
+//Mogą tez jednoczesnie implementować interfejsy i rozszerzac inne klasy
+var OtherClass = /** @class */ (function () {
+    function OtherClass() {
+    }
+    return OtherClass;
+}());
+var MyClass2 = /** @class */ (function (_super) {
+    __extends(MyClass2, _super);
+    function MyClass2() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return MyClass2;
+}(OtherClass));
+//Bardzo często interfejsy opisują konkretne cechy obiektów a nie ich caly kształt.
+//Typ statyczny i typ instancji w klasach
+var User13 = /** @class */ (function () {
+    function User13(id) {
+        this.id = id;
+    }
+    User13.compare = function (user1, user2) {
+        return user1.id === user2.id;
+    };
+    return User13;
+}());
+var userA = new User13("abc");
+var userB = new User13("123");
+var userC = new User13("abc");
+User13.compare(userA, userB); //false
+User13.compare(userA, userC); //true
+var User14 = /** @class */ (function () {
+    function class_1(id) {
+        this.id = id;
+    }
+    class_1.compare = function (a, b) {
+        return a.id === b.id;
+    };
+    return class_1;
+}());
+var user14a = new User14("D");
+var user14b = new User14("D");
+var user14c = new User14("E");
+User14.compare(user14a, user14b);
+console.log(User14.compare(user14c, user14b));
---
+++ 06/index.ts
@@ -0,0 +1,416 @@
+/* Klasy i interfejsy */
+
+    //Klasa
+        class User{
+            name: string;
+            
+            sayHello(){
+                console.log(`My name is ${this.name}`)
+            }
+        }
+    
+        //Wywołanie
+        const x = new User();
+
+        //Deklaracja klasy jest równiez deklaracją nowego typu User
+        const x2: User = new User();
+
+    //Konstruktor
+        //Klasy najczęściej posiadają zdefiniowany konstruktor
+        class User2{
+            name: string;
+
+            constructor(name: string){
+                this.name = name;
+            }
+        }
+
+        //Typescript posiada lukier składniowy upraszczający powyszy kod, oba przyklady są równowane
+        class User3 {
+            constructor(public name: string) {}
+        }
+
+        //Dzięki dodaniu słowa public w konstruktorze przed argumentem name
+        //parametr zostanie automatycznie przypisany do instancji klasy w momencje jej tworzenia
+
+        const x3 = new User3("Michał"); // user.name => 'Michał"
+
+        class User4 {
+            constructor(public name: string, public age: number) {}
+        }
+
+        const x4 = new User4("Adam", 21); // user.name => "Adam", user.age => 21;
+    
+    //Pola bez inicjacji
+        //TS poinformuje nas o pomyłce, jeśli zapomnimy zainicjować jakiegoś pola
+        class User5 {
+            name: string;
+            // Property 'name' has no initializer and is not
+            // definitely assigned in the constructor.(2564)
+        }
+
+        //Aby uniknąć błedu, powinniśmy oznaczyć pole jako opcjonalne
+
+        class User6 {
+            name?: string;
+        }
+
+        //Jeśli chcemy stworzyć pole które nie jest opcjonalne, ale tez nie
+        //jest od razu inicjalizowane, stosujemy kwalifikator  "!"
+        //Ale nie jest to rekomendowane, TS nie będzie w stanie wyłapać pomyłki za nas.
+
+        class User7 {
+            name!: string;
+        }
+
+    //Dziedziczenie
+        //Klasy dziedziczą dzięki słowu kluczowemu extends
+
+        class Vehicle{
+            constructor(public numberOfWheels: number) {}
+        }
+
+        class Car extends Vehicle{
+            ride() {
+                console.log(`I have ${this.numberOfWheels} wheels!`)
+            }
+        }
+
+        const c = new Vehicle(4);
+        //c.ride() // => I have 4 wheels! 
+
+        //Jeśli subklasa równiez definiuje konstruktor, niezbędne
+        //jest uzycie slowa kluczowego 'super'
+
+        class A{
+            a = "test A"
+        }
+
+        class B extends A {
+
+        }
+
+        class C extends A {
+            constructor(){
+                super();
+            }
+        }
+
+    //Modyfikatory public, private, protected
+        //Są podobne jak w innych językach, jednak TS wprowadza drobne róznice.
+        //warta uwaga jest kompatybilność typów przy uzywaniu pól prywatnych
+
+        //public
+            //Oznacza, ze ma się dowolny dostęp do danego pola.
+            //Jeśli nie oznaczymy zadnego modyfikatora, TS przyjmie defaultowo 'public'
+
+            class D{
+                public x: string;
+                //to samo co
+                //x: string;
+            }
+
+        //private
+            //Brak dostępu do danego pola z zewnątrz
+            //Klasy dziedziczące nie mają do niego dostępu
+
+            class User8 {
+                private name: string;
+
+                constructor(name: string){
+                    this.name = name;
+                }
+
+                sayHello() {
+                    console.log(`Jestem ${this.name}`)
+                }
+            }
+
+            const user8 = new User8("Michał");
+            //user8.sayHello(); // => Jestem Michał
+            //user8.name; // => error przy kompilacji
+
+        //protected
+            //To samo co private tylko klasy dziedziczące mają dostęp
+
+            class User9 {
+                protected name: string;
+
+                constructor(name: string){
+                    this.name = name;
+                }
+            }
+
+            class Admin extends User9 {
+                sayHello() {
+                    console.log(this.name)
+                }
+            }
+
+            //const user9 = new Admin("Pablo");
+            //user9.sayHello(); // => "Pablo"
+
+        //readonly
+            //Pola i metody mogą być oznaczone dodatkowo jako readonly, tylko do odczytu
+            //to znaczy, ze nie mozliwe jest przypisanie ich poza konstruktorem
+
+            //Zły przykład, cannot assign to state because read-only property
+            class Component {
+                readonly state = { userId: 123 }
+
+                updateState(){
+                    this.state = {};
+                }
+            }
+
+            //Readonly moze występować obok modyfikatorow dostępu
+            class Component2 {
+                private readonly state = { userId: 123 };
+            }
+
+        //własności w parametrach konstruktora
+            class User10 {
+                name: string;
+
+                constructor(name: string){
+                    this.name = name
+                }
+            }
+
+            //mozemy zamienic na
+
+            class User11 {
+                constructor(public name: string) {}
+            }
+
+            //Analogicznie mozna postepić z innymi modyfikatorami
+
+            class User12{
+                constructor(public readonly name: string) {}
+            }
+    
+    //Klasa Abstrakcyjna
+        //Są to klasy których nie da sie instancjonować i słuza wyłącznie do dziedziczenia po nich.
+        //Klasy te deklarujemy tylko podajac typ i oznacza sie je rowniez slowem "abstract"
+        abstract class Message {
+            constructor(private text: string) {
+            };
+            abstract send(): void;
+
+            log(){
+                console.log(this.text);
+            }
+        }
+
+        class Phone extends Message{
+            sendSms(): void {
+                console.log('xD');
+            }
+        };
+
+        const tel = new Phone('Hello moto');
+        //tel.sendSms(); // => "xD"
+        //tel.log();  // => "Hello moto"
+    
+    //Interfejs
+        //Podstawowy budulec TSa, opisuje kształt czegoś i podlega zasadon typowania strukturalnego.
+        //Interfejs nazywa i definiuje typ bez zadnej implementacji
+
+        interface Invoice{
+            amount: number;
+            submittedAt: Date;
+        }
+
+        //Teraz mozna uzywać tego typu
+
+        const invoice: Invoice = { 
+            amount: 123,
+            submittedAt: new Date(),
+        }
+
+        //Kompatybilność typów
+
+            function getInvoiceSummary(invoice: Invoice){
+                return (
+                    `Twoja faktura na kwote ${invoice.amount}` +
+                    `została wysłana ${invoice.submittedAt}`
+                )
+            }
+
+            getInvoiceSummary(invoice); //all good
+            getInvoiceSummary({
+                amount:42,
+                submittedAt: new Date(),
+            }) // all good
+            getInvoiceSummary({
+                amount:42,
+                submittedAt: new Date(),
+                blabla: 123
+            }) //err
+
+            const moreThanInvoice = {
+                amount:42,
+                submittedAt: new Date(),
+                blabla:123
+            }
+
+            getInvoiceSummary(moreThanInvoice) // all good!
+
+            //Modyfikatory w interfejsach
+                // Własność jest opcjonalna lub tylko do odczytu (? i readonly) 
+
+                interface Example {
+                    optionalProperty?: number;
+                    readonly cannotUpdateMe:string;
+                }
+
+                const ex: Example = { 
+                    cannotUpdateMe: "abc"
+                }
+
+                ex.optionalProperty = 123 // OK => 123
+                ex.cannotUpdateMe = "a" //error
+
+                //Mozliwe jest stworzenie tablicy tylko do odczytu
+                //Wszystkie metody, modifikowanie jest niedozwolone w tego typu tablicy
+
+                const arr: ReadonlyArray<number> = [1,2,3];
+                arr[0] = 42; //err
+                arr.push(123) //err
+                arr.unshift() //err
+
+                const arr2: Array<number> = [1,2,3];
+                arr2[0] = 42; //ok
+                arr2.push(123) //ok
+                arr2.unshift() //ok
+            
+            //Wywoływalne i konstruowalne interfejsy
+                //Interfejsy mogą zostać wykorzystane do opisania funkcji
+
+                //Callable to typ, który opisuje funkcje.
+                interface Callable {
+                    render(name: string): boolean;
+                    inject: Array<string>;
+                }
+
+                //Mozliwe jest równiez opisanie typu, który jest konstruowalny.
+                interface Constructor {
+                    new (): number;
+                }
+
+                //Funkcje w JS mogą być konstruktorami i mogą zwracać co innego
+                interface WeirdFunction {
+                    new (): number;
+                    (): string;
+                }
+
+                //TS musi dawać mozliwość przedstawiania konstrukcji które juz istnieją w JS
+
+                //Czy to poprawny kod? Raczej nie.
+               /* function Date(...args){
+                    if(this instanceof Date){
+                        initializeDate(this, ...args);
+                    }
+
+                    return toDateString(clockGetTime());
+                } */
+
+            //Implementowanie interfejsów w klasach
+                //Mozna implementować interfejsy w klasach
+                interface Queue {
+                    push(val: string): void;
+                    pop(): string;
+                }
+
+                class ArrayQueue implements Queue {
+                    private array: Array<string> = [];
+
+                    push(val: string){
+                        this.array.push(val);
+                    }
+
+                    pop(){
+                        return this.array.pop();
+                    }
+                }
+
+                const arrQue = new ArrayQueue();
+                arrQue.push('Apple');
+                arrQue.push('Pineapple');
+                arrQue.pop(); 
+                arrQue; // ({array}) => "Apple" 
+
+                //Klasy interfejsów mogą implementować wiele interfejsów:
+                class MyClass implements A, B, C {
+                    //...
+                }
+
+                //Mogą tez jednoczesnie implementować interfejsy i rozszerzac inne klasy
+                class OtherClass{}
+                class MyClass2 extends OtherClass implements A, B, C {
+                    //...
+                }
+
+                //Podobnie, interfejsy mogą rozszerzać inne interfejsy
+
+                interface Drawable {
+                    draw(): void;
+                }
+
+                interface Collidable {
+                    readonly x: number;
+                    readonly y: number;
+                    collidesWith(obj: Collidable): boolean;
+                }
+
+                interface Character extends Drawable, Collidable {
+                    readonly health: number;
+                }
+
+                //Bardzo często interfejsy opisują konkretne cechy obiektów a nie ich caly kształt.
+
+            //Typ statyczny i typ instancji w klasach
+
+            class User13 {
+                constructor(public id: string) {}
+
+                static compare(user1: User13, user2: User13): boolean{
+                    return user1.id === user2.id;
+                }
+            }
+
+            const userA = new User13("abc")
+            const userB = new User13("123")
+            const userC = new User13("abc")
+
+            User13.compare(userA, userB) //false
+            User13.compare(userA, userC) //true
+
+            //Do opisania typu statycznego i typu instacji mozna uzyc interfejsow
+
+            interface UserInstance {
+                id: string;
+            }
+
+            interface UserConstructor {
+                new (id: string): UserInstance;
+                compare(a: UserInstance, b: UserInstance): boolean;
+            }
+
+            const User14: UserConstructor = class implements UserInstance {
+                constructor(public id: string) {}
+
+                static compare(
+                    a: UserInstance,
+                    b: UserInstance
+                ): boolean {
+                    return a.id === b.id
+                }
+            };
+
+            const user14a = new User14("D")
+            const user14b = new User14("D")
+            const user14c = new User14("E")
+
+            User14.compare(user14a, user14b)
+            console.log(User14.compare(user14c, user14b))
